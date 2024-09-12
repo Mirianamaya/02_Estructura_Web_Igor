920cf68520139747050fc6bd020ace31c0be4d96
Mirianamaya <mirianamaya@gmail.com>
2024-09-12 20:49:46 +0200

    12/09

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+import "./_scroll.js";
+
+const midocumentoHTML = document;
+midocumentoHTML.addEventListener('DOMContentLoaded',()=>{ 
+    /* todo lo que meta aquí dentro se ejecutará cuando se termine de cargar el DOM */
+});
